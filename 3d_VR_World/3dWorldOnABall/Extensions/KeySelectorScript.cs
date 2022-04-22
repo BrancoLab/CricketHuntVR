037e39ecc2f7d41cbd6b194cb789643e839e5bb9
Bruno Cruz <bfpcruz@gmail.com>
2022-04-22 11:15:15 +0100

    Add demo of a 3d VR world

---
+++ 3d_VR_World/3dWorldOnABall/Extensions/KeySelectorScript.cs
@@ -0,0 +1,28 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using OpenTK.Input;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Condition)]
+
+public class KeySelectorScript
+{
+    public Key[] KeyFilter { get; set; }
+    public IObservable<KeyboardState> Process(IObservable<KeyboardState> source)
+    {
+        return source.Where(value =>
+        {
+            foreach (Key thisKey in KeyFilter)
+            {
+                if (value[thisKey])
+                    return true;
+            }
+            return false;
+        });
+    }
+}
