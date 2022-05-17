b606bf97cd1888ecde1a6fc366b957782ccc93d4
Bruno Cruz <bfpcruz@gmail.com>
2022-05-17 13:04:34 +0100

    cleanup vr workflows

--- 3d_VR_World/3dWorldOnABall/Extensions/CalculateAngle.cs
+++ 3d_VR_World/3dWorldOnABall/Extensions/CalculateAngle.cs
@@ -1,41 +1,41 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using CricketVR;
-using OpenTK;
 
-[Combinator]
-[Description("")]
-[WorkflowElementCategory(ElementCategory.Transform)]
-public class CalculateAngle
+namespace CricketVR
 {
-    public IObservable<Tuple<double,double>> Process(IObservable<VrElement> source)
+    [Combinator]
+    [Description("")]
+    [WorkflowElementCategory(ElementCategory.Transform)]
+    public class CalculateAngle
     {
-        return source.Select(value => {
+        public IObservable<Tuple<double,double>> Process(IObservable<VrElement> source)
+        {
+            return source.Select(value => {
 
-            var mag = value.Position.Length;
+                var mag = value.Position.Length;
 
-            var theta = Math.Acos(value.Position.Z / mag);
-            var phi = Math.Asin(value.Position.Y / mag);
+                var theta = Math.Acos(value.Position.Z / mag);
+                var phi = Math.Asin(value.Position.Y / mag);
 
-            return Tuple.Create(theta, phi);
+                return Tuple.Create(theta, phi);
+                });
+        }
+
+        public IObservable<Tuple<double,double>> Process(IObservable<Tuple<VrElement, VrElement>> source)
+        {
+            return source.Select(value => {
+                var delta_position = (value.Item1 - value.Item2).Position;
+                var mag = delta_position.Length;
+
+                var theta = Math.Acos(delta_position.Z / mag);
+                var phi = Math.Asin(delta_position.Y / mag);
+
+                return Tuple.Create(theta, phi);
             });
-    }
+        }
 
-    public IObservable<Tuple<double,double>> Process(IObservable<Tuple<VrElement, VrElement>> source)
-    {
-        return source.Select(value => {
-            var delta_position = (value.Item1 - value.Item2).Position;
-            var mag = delta_position.Length;
-
-            var theta = Math.Acos(delta_position.Z / mag);
-            var phi = Math.Asin(delta_position.Y / mag);
-
-            return Tuple.Create(theta, phi);
-        });
-    }
-
+}
 }
