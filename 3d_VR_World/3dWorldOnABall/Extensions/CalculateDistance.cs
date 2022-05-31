4bee11e8d10010a783f337331557dbb5e9a75890
aspaNeuro <a.almeida@neurogears.org>
2022-05-31 08:43:54 +0100

    dev branch reorg

--- 3d_VR_World/3dWorldOnABall/Extensions/CalculateDistance.cs
+++ 3d_VR_World/3dWorldOnABall/Extensions/CalculateDistance.cs
@@ -1,31 +1,30 @@
 using Bonsai;
 using System;
 using System.ComponentModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using CricketVR;
 
-namespace CricketVR
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class CalculateDistance
 {
-    [Combinator]
-    [Description("")]
-    [WorkflowElementCategory(ElementCategory.Transform)]
-    public class CalculateDistance
+
+    public IObservable<float> Process(IObservable<VrElement> source)
     {
-
-        public IObservable<float> Process(IObservable<VrElement> source)
-        {
-            return source.Select(value => {
-                return value.Position.Length;
-            });
-        }
-
-        public IObservable<float> Process(IObservable<Tuple<VrElement, VrElement>> source)
-        {
-            return source.Select(value => {
-                VrElement delta = value.Item1 - value.Item2;
-                return delta.Position.Length;
-            });
-        }
-
+        return source.Select(value => {
+            return value.Position.Length;
+        });
     }
+
+    public IObservable<float> Process(IObservable<Tuple<VrElement, VrElement>> source)
+    {
+        return source.Select(value => {
+            VrElement delta = value.Item1 - value.Item2;
+            return delta.Position.Length;
+        });
+    }
+
 }
