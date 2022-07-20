5eb764699452f1eaf0077e74b0e3b85b8120f623
Bruno Cruz <b.cruz@neurogears.org>
2022-07-20 23:08:42 +0100

    Add Logging

---
+++ src/Workflows/Prototyping3dWorldOnABall/Extensions/CricketVR/VRElementToHarpMessagecs.cs
@@ -0,0 +1,38 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using CricketVR;
+using Bonsai.Harp;
+
+namespace CricketVR
+{
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class VRElementToHarpMessagecs
+{
+    const int Address = 201;
+
+    public IObservable<HarpMessage> Process(IObservable<Tuple<VrElement, double>> source)
+    {
+        return source.Select(value => {
+            var vre = value.Item1;
+            var ts = value.Item2;
+            return HarpMessage.FromSingle(
+                Address,
+                ts,
+                MessageType.Event,
+                (float) vre.Position.X,
+                (float) vre.Position.Y,
+                (float) vre.Position.Z,
+                (float) vre.Orientation.X,
+                (float) vre.Orientation.Y,
+                (float) vre.Orientation.Z
+                );
+        });
+    }
+}
+}
