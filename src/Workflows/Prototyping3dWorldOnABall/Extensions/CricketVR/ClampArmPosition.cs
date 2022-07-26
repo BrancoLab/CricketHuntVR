5ee3b08d90a5b804641f305ffbcd59602b50daf4
Bruno Cruz <b.cruz@neurogears.org>
2022-07-26 08:49:19 +0100

    Arm position is now correctly clamped

---
+++ src/Workflows/Prototyping3dWorldOnABall/Extensions/CricketVR/ClampArmPosition.cs
@@ -0,0 +1,51 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using OpenCV.Net;
+
+namespace CricketVR{
+
+[Combinator]
+[Description("Clamps an Arm position between the maximum and minimum radial parameters")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class ClampArmPosition
+{
+    private float minRadius;
+    public float MinRadius
+    {
+        get { return minRadius; }
+        set { minRadius = value; }
+    }
+
+
+    private float maxRadius;
+    public float MaxRadius
+    {
+        get { return maxRadius; }
+        set { maxRadius = value; }
+    }
+
+    public IObservable<Point2f> Process(IObservable<Point2f> source)
+    {
+        return source.Select(value => {
+            var magnitude = (float)(Math.Sqrt(value.X * value.X + value.Y * value.Y));
+            Console.WriteLine(value);
+
+            if ((magnitude  <= maxRadius) & (magnitude >= minRadius)){
+                return value;
+            }
+            else{
+                var Angle = (float)(Math.Atan2(value.Y, value.X));
+                magnitude = magnitude > maxRadius ? maxRadius : (magnitude < minRadius ? minRadius : magnitude);
+                return new Point2f(
+                    (float) Math.Cos(Angle) * magnitude,
+                    (float) Math.Sin(Angle) * magnitude
+                );
+            }
+        });
+    }
+}
+}
