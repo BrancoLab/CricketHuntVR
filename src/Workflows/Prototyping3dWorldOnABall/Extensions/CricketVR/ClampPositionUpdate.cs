4f93ab9c81c2f36d4d79cc3da34b730eb45bdc90
Bruno Cruz <b.cruz@neurogears.org>
2022-07-27 15:56:59 +0100

    Fix position update when using the mouse input; fix arena limits

--- src/Workflows/Prototyping3dWorldOnABall/Extensions/CricketVR/ClampPositionUpdate.cs
+++ src/Workflows/Prototyping3dWorldOnABall/Extensions/CricketVR/ClampPositionUpdate.cs
@@ -12,7 +12,7 @@ namespace CricketVR
     public class ClampPositionUpdate
     {
 
-        private float numericCorrectionFactor = 0.99f;
+        private float numericCorrectionFactor = 1f;
         [Description("Correction factor to adjust the numeric bounds. [0:1]")]
         public float NumericCorrectionFactor
         {
@@ -27,6 +27,22 @@ namespace CricketVR
         }
 
 
+        private Vector3 offsetBounds = new Vector3(0);
+        [TypeConverter(typeof(NumericRecordConverter))]
+        [Description("Offsets the rectangular arena")]
+        public Vector3 OffsetBounds
+        {
+            get
+            {
+                return offsetBounds;
+            }
+            set
+            {
+                offsetBounds = value;
+            }
+        }
+
+
         // Full VrElement input
         public IObservable<VrElement> Process(IObservable<Tuple<VrElement, VrElement, Tuple<float, float, float, float, float, float>>> source)
         {
@@ -36,7 +52,7 @@ namespace CricketVR
                 var Accumulation = value.Item1;
                 var Update = value.Item2;
                 var ClampedUpdate = Update;
-                var bounds = value.Item3;
+                var bounds = ApplyOffset(value.Item3, offsetBounds);
 
                 // Update the position
                 Accumulation.Position.X = ClampPosition(
@@ -67,7 +83,7 @@ namespace CricketVR
                 var Accumulation = value.Item1;
                 var Update = value.Item2;
                 var ClampedUpdate = Update;
-                var bounds = value.Item3;
+                var bounds = ApplyOffset(value.Item3, offsetBounds);
 
                 // Update the position
                 Accumulation.X = ClampPosition(
@@ -90,11 +106,52 @@ namespace CricketVR
             });
         }
 
+        public IObservable<Vector3> Process(IObservable<Tuple<Vector3, Tuple<float, float, float, float, float, float>>> source)
+        {
+            return source.Select(value =>
+            {
+                var Accumulation = value.Item1;
+                var bounds = ApplyOffset(value.Item2, offsetBounds);
+
+                // Update the position
+                Accumulation.X = ClampPosition(
+                    Accumulation.X,
+                    bounds.Item1 * NumericCorrectionFactor,
+                    bounds.Item2 * NumericCorrectionFactor);
+
+                Accumulation.Y = ClampPosition(
+                    Accumulation.Y,
+                    bounds.Item3 * NumericCorrectionFactor,
+                    bounds.Item4 * NumericCorrectionFactor);
+
+                Accumulation.Z = ClampPosition(
+                    Accumulation.Z,
+                    bounds.Item5 * NumericCorrectionFactor,
+                    bounds.Item6 * NumericCorrectionFactor);
+
+                return (Accumulation);
+
+            });
+        }
+
         public static float ClampPosition(float In, float max, float min)
         {
             if (In < min) return min;
             else if (In > max) return max;
             else return In;
+        }
+
+        public static Tuple<float, float, float, float, float, float> ApplyOffset(Tuple<float, float, float, float, float, float> bounds, Vector3 offset)
+        {
+
+            return Tuple.Create(
+                bounds.Item1 + offset.X,
+                bounds.Item2 - offset.X ,
+                bounds.Item3 + offset.Y,
+                bounds.Item4 - offset.Y,
+                bounds.Item5 + offset.Z,
+                bounds.Item6 - offset.Z
+                );
         }
     }
 }
