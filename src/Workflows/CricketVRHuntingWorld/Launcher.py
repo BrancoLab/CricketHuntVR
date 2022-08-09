9610f8877ab527bbfffdc7d9e799ff19a3cbcd7b
Bruno Cruz <b.cruz@neurogears.org>
2022-08-09 13:11:56 +0100

    add udp mode to Launcher

--- src/Workflows/CricketVRHuntingWorld/Launcher.py
+++ src/Workflows/CricketVRHuntingWorld/Launcher.py
@@ -8,6 +8,7 @@ import subprocess
 
 from pathlib import Path
 from datetime import datetime
+from time import sleep
 
 ## User Settings
 UDP_Mode = False
@@ -15,6 +16,8 @@ UDP_Mode = False
 Settings = {
 "Logging.LoggingRootPath" : r"C:\Users\Cricket Team\Desktop\data"
 }
+
+UDP_Settings = {}
 
 ## Launch Bonsai
 
@@ -26,11 +29,9 @@ bonsai_path = cwd.parents[2] / "Bonsai" / "Bonsai.exe"
 if ~UDP_Mode:
     workflow_path = cwd / "CricketVRHuntingWorld.bonsai"
     LayoutFile = cwd / "DefaultCricketVRHuntingWorld.bonsai.layout"
-
 else:
-    workflow_path = cwd / "CricketVRHuntingWorld.bonsai"
-    LayoutFile = cwd / "DefaultCricketVRHuntingWorld.bonsai.layout"
-
+    workflow_path = cwd / "CricketVRHuntingWorld_NoFakeWorld.bonsai"
+    LayoutFile = cwd / "DefaultCricketVRHuntingWorld_NoFakeWorld.bonsai.layout"
     udp_workflow_path = cwd / "DrawWorld.bonsai"
     udp_LayoutFile = cwd / "default_udp_drawworld.bonsai.layout"
 
@@ -45,6 +46,18 @@ _layout = LayoutFile
 if not(_layout == ""):
     output_cmd += f' --visualizer-layout:"{_layout}"'
 
-print(output_cmd)
+print(f"Starting... {output_cmd}")
 
 bonsai_process = subprocess.Popen(output_cmd, cwd=cwd, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+if UDP_Mode:
+    sleep(5)
+    udp_draw_output_cmd = f'"{bonsai_path}" "{udp_workflow_path}" --no-editor'
+    for sett in UDP_Settings.keys():
+        udp_draw_output_cmd += f' -p:{sett}="{UDP_Settings[sett]}"'
+
+    if not(udp_LayoutFile == ""):
+        udp_draw_output_cmd += f' --visualizer-layout:"{udp_LayoutFile}"'
+
+    print(f"Starting... {udp_draw_output_cmd}")
+    draw_process = subprocess.Popen(udp_draw_output_cmd, cwd=cwd, creationflags=subprocess.CREATE_NEW_CONSOLE)
